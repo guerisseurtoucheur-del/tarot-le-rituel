649a2b3454088dc6d2f469c1da38eae749b01aff
guerisseurtoucheur-del <guerisseurtoucheur@gmail.com>
2026-01-25 18:08:46 +0100

    feat: Migrate from Jekyll to Vite and React
    This commit completely refactors the project from a Jekyll-based static site to a Vite-powered React application. This includes updating the entry point, build configuration, and CI/CD pipeline to align with the new stack. The base URL configuration has also been removed as it's no longer necessary with the Vite setup.

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -6,9 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      // AJOUT ICI : Indique à Vite le nom de ton dépôt GitHub
-      base: '/tarot-le-rituel/', 
-      
       server: {
         port: 3000,
         host: '0.0.0.0',
